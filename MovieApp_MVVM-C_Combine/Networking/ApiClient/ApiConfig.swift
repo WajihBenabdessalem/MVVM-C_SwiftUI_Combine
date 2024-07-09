17e0138c914b51cb409c52b120a2a03098286f01
Wajih Benabdessalem <wajih.abdes@gmail.com>
2024-07-09 02:12:17 +0200

    Clean code

--- MovieApp_MVVM-C_Combine/Networking/ApiClient/ApiConfig.swift
+++ MovieApp_MVVM-C_Combine/Networking/ApiClient/ApiConfig.swift
@@ -9,6 +9,6 @@ import Foundation
 
 struct ApiConfig {
     static let imageBaseURL = "https://image.tmdb.org/t/p/original/"
-    static let apiKey = "7316b384dfd4101c86646d969218d3e1"
+    static let apiKey = "Your_Api_Key"
     static let mockURL = "https://invalid-url"
 }
